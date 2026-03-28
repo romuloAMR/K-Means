01d990019220ed088289dc9b47968264d5bdf422
romuloAMR <amr.rmulo@gmail.com>
2026-03-28 19:33:49 +0000

    feat: create k-means v2 in java

--- java/v2/src/main/java/com/kmeans/Main.java
+++ java/v2/src/main/java/com/kmeans/Main.java
@@ -1,7 +1,18 @@
 package com.kmeans;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.err.println("Love, Faith, and Hope - V2");
+        
+        try {
+            List<Point> points = CSVReader.loadPoints("../data/dataset_1000000x100_range_0.0_to_100.0.csv");
+            Kmeans ai = new Kmeans(3, points, 2026);
+            ai.fit();
+            //System.err.printf("Centroids: %s%n", ai.getCentroids());
+            //System.err.printf("Clusters: %s%n", ai.getClusters());
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
