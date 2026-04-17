bf8246c9b4123b8b107980b327b316603bbb8101
romuloAMR <amr.rmulo@gmail.com>
2026-04-17 15:17:50 +0000

    feat: apply file segmentation for reading

--- java/v3/src/main/java/com/kmeans/Main.java
+++ java/v3/src/main/java/com/kmeans/Main.java
@@ -1,12 +1,13 @@
 package com.kmeans;
 
+import java.nio.file.Paths;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         
         try {
-            List<Point> points = CSVReader.loadPoints("../data/dataset_1000000x100_range_0.0_to_100.0.csv");
+            List<Point> points = CSVReader.loadPoints(Paths.get("../data/dataset_1000000x100_range_0.0_to_100.0.csv"));
             Kmeans ai = new Kmeans(3, points, 2026);
             ai.fit();
             //System.err.printf("Centroids: %s%n", ai.getCentroids());
